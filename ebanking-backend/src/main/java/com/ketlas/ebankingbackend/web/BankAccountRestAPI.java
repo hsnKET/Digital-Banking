374fb19f71cec0e7f787874ca7c97e014cb74978
Hassan Ketlas <hsn.ketlas@gmail.com>
2022-05-22 20:13:14 +0100

    Initial Component

--- ebanking-backend/src/main/java/com/ketlas/ebankingbackend/web/BankAccountRestAPI.java
+++ ebanking-backend/src/main/java/com/ketlas/ebankingbackend/web/BankAccountRestAPI.java
@@ -8,15 +8,13 @@ import com.ketlas.ebankingbackend.exceptions.BankAccountNotFoundException;
 import com.ketlas.ebankingbackend.services.BankAccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class BankAccountRestAPI {
 
 
