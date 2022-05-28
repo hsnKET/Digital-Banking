1b3901c2fcd28b442111baf4a5d481e9f3414ee8
ketlasdev <hsn.ketlas@gmail.com>
2022-05-28 14:03:51 +0100

    JWT authentication Added

--- ebanking-backend/src/main/java/com/ketlas/ebankingbackend/security/SecurityConfig.java
+++ ebanking-backend/src/main/java/com/ketlas/ebankingbackend/security/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.ketlas.ebankingbackend.security;
 
+import com.ketlas.ebankingbackend.security.filter.JwtAuthenticationFilter;
+import com.ketlas.ebankingbackend.security.filter.JwtAuthorizationFilter;
 import com.ketlas.ebankingbackend.security.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -22,21 +28,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsServiceImpl userDetailsService;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
         auth.userDetailsService(userDetailsService);
-
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         //http.formLogin();
-        http.cors().disable();
-        http.headers().frameOptions().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.cors().disable();
         http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.headers().frameOptions().disable();
+        http.addFilter(new JwtAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new JwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.authorizeRequests().antMatchers(HttpMethod.POST,"/users/**").hasAuthority("ADMIN");
+
         http.authorizeRequests().anyRequest().authenticated();
 
+
+    }
+
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 
 
